f0cc51922259a4d5b17680c0d7745764fe465ab0
Bryan <alelinapps.com>
2022-08-28 07:32:40 -0700

    Initial commit

--- iOS16Problems/iOS16ProblemsApp.swift
+++ iOS16Problems/iOS16ProblemsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iOS16ProblemsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListSecHeaderHeightView()
         }
     }
 }
